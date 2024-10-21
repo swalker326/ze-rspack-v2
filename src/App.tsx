42416a574e9098f41c287b032c2e7aac42b2b40a
Shane <shane@swalker.dev>
2024-10-21 15:00:36 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+    <div className="App">
+      <h1>I'm an RSPACK app woot</h1>
+      <h1>I'm an RSPACK app woot</h1>
+    </div>
+  );
+}
+
+export default App;
